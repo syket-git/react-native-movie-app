d7d621a7a653bead11a8092e151fc43df4b5197c
syket <syketweb@gmail.com>
2024-02-24 10:38:53 +0600

    first commit

---
+++ navigation/appNavigation.js
@@ -0,0 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import * as React from "react";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
